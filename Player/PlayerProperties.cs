5bf893c1238e1c66f52754817893977c30b8f3f4
Unknown <kirby94@hotmail.it>
2017-10-22 15:13:29 +0200

    - Rename to PlayerMovement.cs

--- Player/PlayerProperties.cs
+++ Player/PlayerProperties.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerInformation : MonoBehaviour {
+public class PlayerProperties : MonoBehaviour {
     public float rotationSpeed;
     public float forwardSpeed;
     public float maxForwardSpeed;
