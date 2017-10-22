b3c1c8ee1f4d1b4474d7c49ba6156bf61377f9ea
Unknown <kirby94@hotmail.it>
2017-10-22 15:03:23 +0200

    - Rename LevelMaker.cs to LevelGenerator.cs

--- Controllers/LevelGenerator.cs
+++ Controllers/LevelGenerator.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LevelMaker : MonoBehaviour {
+public class LevelGenerator : MonoBehaviour {
 
     // Private
     GameSettings gameSettings;
