6866d0e67566bdcd00aa9486fefd955d7813506d
Unknown <kirby94@hotmail.it>
2017-10-22 22:53:47 +0200

    BlackHoleController.cs - Open and Close visually black hole

---
+++ BlackHole/BlackHoleController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackHoleController : MonoBehaviour {
+
+    public float startingSize;
+    public float finalSize;
+    public float growingDuration;
+
+	void Start () {
+        StartCoroutine(blackHoleLife());
+	}
+
+    IEnumerator blackHoleLife()
+    {
+        yield return StartCoroutine(changeSize(startingSize, finalSize));
+        yield return new WaitForSeconds(2f);
+        yield return StartCoroutine(changeSize(finalSize, startingSize));
+    }
+
+    IEnumerator changeSize(float startingSize, float finalSize)
+    {
+
+        float addend;
+
+        if( startingSize <= finalSize )
+        {
+            addend = (finalSize - startingSize)*Time.deltaTime / growingDuration;
+
+            for (float actualSize = startingSize; actualSize < finalSize; actualSize += addend) {
+                transform.localScale = new Vector3(actualSize, actualSize, 1);
+                yield return null;
+            }
+        }
+        else
+        {
+            addend = (startingSize - finalSize) * Time.deltaTime / growingDuration;
+            print(addend);
+            for (float actualSize = startingSize; actualSize > finalSize; actualSize -= addend)
+            {
+                transform.localScale = new Vector3(actualSize, actualSize, 1);
+                yield return null;
+            }
+        }
+
+    }
+
+}
