97cff1b7a4d6dedfa82c657587105947e3a82e71
Unknown <kirby94@hotmail.it>
2017-10-23 19:06:00 +0200

    BlackHoleCollisionController.cs - Teleport objects that collide with the black hole

---
+++ BlackHole/BlackHoleCollisionController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackHoleCollisionController : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        print("ENTER");
+        other.gameObject.transform.position = new Vector3(-10,-10,0);
+    }
+}
