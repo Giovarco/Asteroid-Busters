cbf3bc42a384ae919572eb2887fc3ab3a4c96a28
Unknown <kirby94@hotmail.it>
2017-10-23 17:38:56 +0200

    SpriteFlash.cs - Script to change flash value over time

---
+++ Generic/SpriteFlash.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteFlash : MonoBehaviour {
+
+    public Material defaultColorFlash;
+
+    Renderer renderer;
+
+    void Awake()
+    {
+        renderer = gameObject.GetComponent<Renderer>();
+        renderer.material = new Material(defaultColorFlash);
+    }
+
+    public IEnumerator changeFlash(float startingValue, float finalValue, float changeDuration)
+    {
+
+        float addend;
+
+        if (startingValue <= finalValue)
+        {
+            addend = (finalValue - startingValue) * Time.deltaTime / changeDuration;
+
+            for (float actualValue = startingValue; actualValue < finalValue; actualValue += addend)
+            {
+                renderer.material.SetFloat("_FlashAmount", actualValue);
+                yield return null;
+            }
+        }
+        else
+        {
+            addend = (startingValue - finalValue) * Time.deltaTime / changeDuration;
+            print(addend);
+            for (float actualValue = startingValue; actualValue > finalValue; actualValue -= addend)
+            {
+                renderer.material.SetFloat("_FlashAmount", actualValue);
+                yield return null;
+            }
+        }
+
+    }
+
+}
