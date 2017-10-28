c565d9e1e096feecce5ef122eeeb48901ed38900
Unknown <kirby94@hotmail.it>
2017-10-28 13:53:52 +0200

    - Clear code

--- Controllers/GameSettings.cs
+++ Controllers/GameSettings.cs
@@ -33,18 +33,6 @@ public class GameSettings : MonoBehaviour {
 
     float screenRatio;
 
-    // Asteroid configuration
-    /*
-    [Header("Asteroid configuration")]
-    public float baseAsteroidSpeed;
-
-    [Tooltip("2 = 50%, 4 = 25% etc.")]
-    public float sizeReductionFactor;
-
-    [Tooltip("This variable prevents all the asteroid to have all the same speed (not intended to exceed maximum speed)")]
-    public float speedVariance;
-    */
-
     // Asteroid difficulty factors
     [Header("Difficulty configuration")]
 
