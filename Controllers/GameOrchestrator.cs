cf698a26acc10c7ecb54d8658d741585e6039f8d
Unknown <kirby94@hotmail.it>
2017-11-02 09:27:07 +0100

    - Use coroutine to check if the each level is completed

--- Controllers/GameOrchestrator.cs
+++ Controllers/GameOrchestrator.cs
@@ -24,16 +24,22 @@ public class GameOrchestrator : MonoBehaviour {
         blackHoleFactory = GameObject.Find("Factories").GetComponent<BlackHoleFactory>();
     }
 
+    void OnEnable()
+    {
+        EventManager.StartListening("AsteroidDestroyed", isLevelCompletedWrapper);
+        print("GameOrchestrator: subscribed to AsteroidDestroyed");
+    }
+
     void Start()
     {
-        StartCoroutine(startGameOrchestration());
+        StartCoroutine(generateLevel());
         StartCoroutine(spawnBlackHole());
     }
 
     bool asteroidsExist()
     {
         int childCount = asteroidContainer.transform.childCount;
-
+        print("GameOrchestrator: childcount = "+childCount);
         if (childCount == 0)
         {
             return false;
@@ -42,6 +48,52 @@ public class GameOrchestrator : MonoBehaviour {
         {
             return true;
         }
+    }
+
+    IEnumerator isLevelCompleted()
+    {
+        yield return null;
+        print("GameOrchestrator: isLevelCompleted()");
+        print("GameOrchestrator: asteroidsExist() = " + asteroidsExist());
+        print("GameOrchestrator: generateLevel() = " + generateLevel());
+        // If there are no more asteroid, start a new level
+        if (!asteroidsExist())
+        {
+            print("GameOrchestrator: there are no more asteroids! I'll generate a new level!");
+            StartCoroutine(generateLevel());
+        }
+    }
+
+    void isLevelCompletedWrapper()
+    {
+        StartCoroutine(isLevelCompleted());
+    }
+
+    IEnumerator generateLevel()
+    {
+        print("GameOrchestrator: generateLevel()");
+        // Show the text with a the new level value
+        levelGenerator.currentLevel++;
+        levelText.GetComponent<UnityEngine.UI.Text>().text = "Level " + levelGenerator.currentLevel;
+        levelText.SetActive(true);
+
+        // Set the new asteroid sprite
+        asteroidFactory.setNextSprite();
+
+        // Wait some seconds
+        yield return new WaitForSeconds(4f);
+
+        // Deactive the level text
+        levelText.SetActive(false);
+
+        // Wait a bunch of other seconds
+        yield return new WaitForSeconds(1.5f);
+
+        // Generate first level
+        levelGenerator.generateLevel();
+
+        yield return new WaitForSeconds(0.5f);
+
     }
 
     IEnumerator spawnBlackHole()
@@ -53,41 +105,4 @@ public class GameOrchestrator : MonoBehaviour {
         }
     }
 
-    
-    IEnumerator startGameOrchestration()
-    {
-
-        while(true)
-        {
-
-            // If there are no more asteroid, start a new level
-            if (!asteroidsExist())
-            {
-
-                // Show the text with a the new level value
-                levelGenerator.currentLevel++;
-                levelText.GetComponent<UnityEngine.UI.Text>().text = "Level " + levelGenerator.currentLevel;
-                levelText.SetActive(true);
-
-                // Set the new asteroid sprite
-                asteroidFactory.setNextSprite();
-
-                // Wait some seconds
-                yield return new WaitForSeconds(4f);
-
-                // Deactive the level text
-                levelText.SetActive(false);
-
-                // Wait a bunch of other seconds
-                yield return new WaitForSeconds(1.5f);
-
-                // Generate first level
-                levelGenerator.generateLevel();
-            }
-
-            yield return new WaitForSeconds(0.5f);
-
-        }
-
-    }
 }
